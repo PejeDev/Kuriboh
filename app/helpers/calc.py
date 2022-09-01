f6ca92273e915a048595665c1e80a0f3decac3af
PejeDev <hi@pejedev.xyz>
2022-08-31 20:34:24 -0500

    chore: fix lint erros on all .py files

--- app/helpers/calc.py
+++ app/helpers/calc.py
@@ -1,10 +1,13 @@
-def get_smallest_positive_integer_not_in_list(numbers: list[int]) -> int: # Complexity: O(2n) = O(n)
-    num_set = set(numbers) # O(n)
-    min_num = 2
-    
-    if 1 not in num_set: # O(1)
-      return 1
+""" Calculations helper. """
 
-    while min_num in num_set: # O(n)
-       min_num += 1
+def get_smallest_positive_integer_not_in_list(numbers: list[int]) -> int:
+    """ Calculate the smallest positive integer not in a list of numbers. """
+    num_set = set(numbers)  # O(n)
+    min_num = 2
+
+    if 1 not in num_set:  # O(1)
+        return 1
+
+    while min_num in num_set:  # O(n)
+        min_num += 1
     return min_num
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,9 +1,13 @@
+""" Tests for the calculations helper. """
+
 from app.helpers.calc import get_smallest_positive_integer_not_in_list
 
+
 def test_get_smallest_positive_integer_not_in_list():
-		assert get_smallest_positive_integer_not_in_list([1, 3, 2]) == 4
-		assert get_smallest_positive_integer_not_in_list([-1, -3]) == 1
-		assert get_smallest_positive_integer_not_in_list([-999, -1, -3]) == 1
-		assert get_smallest_positive_integer_not_in_list([]) == 1
-		assert get_smallest_positive_integer_not_in_list([1]) == 2
-		assert get_smallest_positive_integer_not_in_list([1, 2, 3, 4, 5, 6, 8, 9, 10]) == 7
+    """ Test the get_smallest_positive_integer_not_in_list function. """
+    assert get_smallest_positive_integer_not_in_list([1, 3, 2]) == 4
+    assert get_smallest_positive_integer_not_in_list([-1, -3]) == 1
+    assert get_smallest_positive_integer_not_in_list([-999, -1, -3]) == 1
+    assert get_smallest_positive_integer_not_in_list([]) == 1
+    assert get_smallest_positive_integer_not_in_list([1]) == 2
+    assert get_smallest_positive_integer_not_in_list([1, 2, 3, 4, 5, 6, 8, 9]) == 7
