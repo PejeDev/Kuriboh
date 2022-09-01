1ebc6b4e54a33977f09f5c4797c56b1123b1d1f3
PejeDev <hi@pejedev.xyz>
2022-09-01 01:17:35 -0500

    feat: get smallest integer endpoint

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -1,10 +1,25 @@
 """ Flask routes handler. """
-
-from flask import jsonify
+from flask import jsonify, request, make_response
+from flask_expects_json import expects_json
+from jsonschema import ValidationError
+from app.helpers.calc import get_smallest_positive_integer_not_in_list
 
 
 def configure_routes(app):
     """ Configures the routes for the application. """
+
+    schema = {
+        'type': 'object',
+        'properties': {
+            'array': {
+                'type': 'array',
+                'items': {
+                    'type': 'integer'
+                }
+            }
+        },
+        'required': ['array']
+    }
 
     @app.route('/api/health', methods=['GET'])
     def get_health():
@@ -12,3 +27,20 @@ def configure_routes(app):
         return jsonify({
             'status': 'ok'
         }), 200
+
+    @app.route('/api/smallest', methods=['POST'])
+    @expects_json(schema)
+    def get_smallest_positive_interger():
+        """ Smallest positive integer not in list endpoint. """
+        data = request.get_json()
+        return jsonify({
+            'result': get_smallest_positive_integer_not_in_list(data['array'])
+        }), 200
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        """ Bad request error handler. """
+        if isinstance(error.description, ValidationError):
+            original_error = error.description
+            return make_response(jsonify({'error': original_error.message}), 401)
+        return make_response(jsonify({'error': "bad request"}), 401)
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -20,3 +20,21 @@ def test_health_route():
     url = '/api/health'
     response = client.get(url)
     assert response.status_code == 200
+
+
+def test_get_smallest_positive_interger_route():
+    """ Test the get_smallest_positive_interger route. """
+    client = app.test_client()
+    url = '/api/smallest'
+    response = client.post(url, json={'array': [-1, -3]})
+    assert response.status_code == 200
+    assert response.json['result'] == 1
+    response = client.post(url, json={'list': [-1, -3]})
+    assert response.status_code == 401
+    assert response.json['error'] == "'array' is a required property"
+    response = client.post(url, json={'array': ["-1", -3]})
+    assert response.status_code == 401
+    assert response.json['error'] == "'-1' is not of type 'integer'"
+    response = client.post(url)
+    assert response.status_code == 401
+    assert response.json['error'] == "bad request"
